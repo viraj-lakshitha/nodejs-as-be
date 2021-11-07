cf1867110bd6712b23a60e64d593c3687e820050
Viraj Lakshitha Bandara <virajlakshithabandara@gmail.com>
2021-11-07 11:22:11 +0530

    Handling Errors

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // Filter Routes with /products, goes with productRoutes
 app.use('/products', productRoutes);
 
 // Filter Routes with /orders, goes with productRoutes
 app.use('/orders', orderRoutes);
+
+// Handling Errors
+// 404 - Not Found Error Handling
+app.use((req, res, next) => {
+	const error = new Error('Not Found!');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	});
+});
 
 module.exports = app;
