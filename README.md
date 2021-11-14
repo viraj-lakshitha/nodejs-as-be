76fc610a2eb425111cdfac367b04d0fc01fb04b4
Viraj Lakshitha Bandara <virajlakshithabandara@gmail.com>
2021-11-14 11:03:42 +0530

    Implement JWT Token

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 ### Install Required Dependecies
 
 - `npm init` to Initialize the package.json file
-- Install packages to the project `npm install --save express nodemon body-parser morgan mongoose dotenv multer bcrypt`
+- Install packages to the project `npm install --save express nodemon body-parser morgan mongoose dotenv multer bcrypt jsonwebtoken`
 
 ### To Run the Project
 
 - Make sure to install the NPM and Setup .env file with required properties
 - Open Terminal in Project and run `npm start`
+
+### Kudos
+- JsonWebToken Repo : https://github.com/auth0/node-jsonwebtoken
+- BCrypt Repo : https://github.com/kelektiv/node.bcrypt.js
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const user = require("../models/user");
 const bcrypt = require("bcrypt");
 const bcryptRounds = Number(process.env.BCRYPT_ROUND);
+const jwt = require("jsonwebtoken")
+const jwtSecretKey = process.env.JWT_KEY
 
 const router = express.Router();
 const User = require("../models/user");
@@ -46,6 +48,45 @@ router.post("/signup", (req, res, next) => {
         })
     })
 });
+
+router.post('/login', (req, res, next) => {
+    User.findOne({email: req.body.email}).exec().then(user => {
+        if (user.length === 0) {
+            res.status(401).json({
+                errorMessage: "Auth Failed!",
+              });
+        }
+
+        bcrypt.compare(req.body.password, user.password, (err, resp) => {
+            if (err) {
+                res.status(401).json({
+                    errorMessage: "Auth Failed!",
+                  });
+            }
+
+            if (resp) {
+                const jwtToken = jwt.sign({
+                    email: user.email,
+                    userId: user._id
+                }, jwtSecretKey, {
+                    expiresIn: "1h"
+                })
+                return res.status(200).json({
+                    message: "Auth Success!",
+                    token: jwtToken
+                  });
+            }
+            res.status(401).json({
+                errorMessage: "Auth Failed!",
+              });
+        })
+
+    }).catch(err => {
+        res.status(500).json({
+            errorMessage: "Something went wrong!"
+        })
+    })
+})
 
 router.delete('/:email', (req, res, next) => {
     User.deleteOne({email: req.params.email}).exec().then(doc => {
